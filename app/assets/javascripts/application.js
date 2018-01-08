f7b7b6616c7100440c09d5a015f744debae03102
Ghislain Chavanne <gh.chavanne@gmail.com>
2018-01-08 08:56:41 +0100

    better advanced

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,15 @@
 //= require_tree .
 
 
+
+
+$( document ).ready(function() {
+  $(document).on('click', '#btn', function(event){
+      event.preventDefault();
+      var offset = - 30;
+
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top + offset
+      }, 500);
+  });
+});
